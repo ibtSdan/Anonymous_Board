5073cf2c184e91a576f84cf73d4339bc711c5d03
ibtsdan <ibtsdan@gmail.com>
2024-11-25 19:28:43 +0900

    GlobalException

--- src/main/java/com/example/anonymous_board/exceptionHandler/GlobalExceptionHandler.java
+++ src/main/java/com/example/anonymous_board/exceptionHandler/GlobalExceptionHandler.java
@@ -1,6 +1,5 @@
 package com.example.anonymous_board.exceptionHandler;
 
-import com.example.anonymous_board.exception.NoChangesException;
 import com.example.anonymous_board.post.common.Api;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.annotation.Order;
@@ -12,21 +11,22 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 import java.util.List;
 
 @RestControllerAdvice
+@Order(value = Integer.MAX_VALUE) // 가장 마지막
 @Slf4j
-@Order(1)
-public class NoChangesExceptionHandler {
-    @ExceptionHandler(value = {NoChangesException.class})
-    public ResponseEntity<Api> noChangesException(NoChangesException e){
+
+public class GlobalExceptionHandler {
+    @ExceptionHandler(value = { Exception.class })
+    public ResponseEntity<Api> globalException(Exception e){
         log.error(""+e);
         var errorList = List.of(e.getMessage());
         var error = Api.Error.builder()
                 .errorMessage(errorList)
                 .build();
         var response = Api.builder()
-                .resultCode(String.valueOf(HttpStatus.BAD_REQUEST.value()))
-                .resultMessage(HttpStatus.BAD_REQUEST.getReasonPhrase())
+                .resultCode(String.valueOf(HttpStatus.INTERNAL_SERVER_ERROR.value()))
+                .resultMessage(HttpStatus.INTERNAL_SERVER_ERROR.getReasonPhrase())
                 .error(error)
                 .build();
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(response);
     }
 }
